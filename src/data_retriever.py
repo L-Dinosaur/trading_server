fe5447775aae449952b078e15919d41227f67861
Dino <hao.dino.liu@gmail.com>
2024-03-10 11:25:24 -0400

    Finalizing data retriever and write unittest for it

--- src/data_retriever.py
+++ src/data_retriever.py
@@ -4,40 +4,37 @@ import pandas as pd
 
 class DataRetriever(object):
 
-    def __init__(self, key, url, interval, tickers):
+    def __init__(self, key, url, interval):
         self.params = dict()
         self.params['key'] = key
-        self.params['url'] = url
+        self.params['base_url'] = url
         self.params['interval'] = interval
-        self.tickers = tickers
 
     def build_url(self, ticker):
-        url = self.params['url'] + '&'.join([param_key + '=' + param for param_key, param in self.params.items()
-                                             if param_key != 'url']) + '&symbol=' + ticker
+        url = self.params['base_url'] + '&'.join([param_key + '=' + param for param_key, param in self.params.items()
+                                                  if param_key != 'url']) + '&symbol=' + ticker
         return url
 
-    def preprocess(self, r, ticker):
-        print("Warning: called unimplemented parent class function.")
-        return pd.DataFrame()
+    def retrieve(self, ticker):
+        """ Retrieve raw data using ticker information and api details stored in the class"""
+        r = requests.get(self.build_url(ticker))
+        return r.json()
 
-    def retrieve(self):
-        urls = {ticker: self.build_url(ticker) for ticker in self.tickers}
-        dfs = [self.preprocess(requests.get(url), ticker) for ticker, url in urls.items()]
-        df = pd.concat(dfs)
-        return df
+    def process_data(self, data, ticker):
+        """ Process raw data using API specific formatting logic, implemented in sub-classes """
+        pass
 
 
 class DataRetrieverAV(DataRetriever):
 
-    def __init__(self, key, url, interval, tickers):
-        super(DataRetrieverAV, self).__init__(key, url, interval, tickers)
+    def __init__(self, key, url, interval):
+        super(DataRetrieverAV, self).__init__(key, url, interval)
         self.params['function'] = 'TIME_SERIES_INTRADAY'
         self.params['outputsize'] = 'full'
         self.params['extended_hours'] = 'false'
         self.params['apikey'] = self.params.pop('key')
 
-    def preprocess(self, r, ticker):
-        data = r.json()
+    def process_data(self, data, ticker):
         df = pd.DataFrame(data["Time Series ({0})".format(self.params['interval'])]).transpose()
         df.drop(["1. open", "2. high", "3. low", "5. volume"], axis=1, inplace=True)
         df.rename({"4. close": "price"}, axis=1, inplace=True)
@@ -48,20 +45,20 @@ class DataRetrieverAV(DataRetriever):
 
 class DataRetrieverFH(DataRetriever):
 
-    def __init__(self, key, url, interval, tickers, fill_logic='flat_fill'):
-        super(DataRetrieverFH, self).__init__(key, url, interval, tickers)
+    def __init__(self, key, url, interval, fill_logic='flat_fill'):
+        super(DataRetrieverFH, self).__init__(key, url, interval)
         self.fill_logic = fill_logic
         self.params['token'] = self.params.pop('key')
 
-    def preprocess(self, r, ticker):
-        """ flatfill or interpolate price data"""
-        data = r.json()
-        # TODO: account for EST/EDT
+    def process_data(self, data, ticker):
+        """ flatfill data"""
+        # TODO: might need to account for EST/EDT
         quote_time = pd.to_datetime(data['t'], unit='s', utc=True).tz_convert('EST').tz_localize(None)
         # assume regular trading window
         beginning = pd.to_datetime(quote_time.strftime('%Y-%m-%d') + ' 09:30', format='%Y-%m-%d %H:%M')
         df_index = pd.date_range(beginning, quote_time, freq=self.params['interval'])  # TODO: address the EOD entry
         df = pd.DataFrame(index=df_index)
+        # TODO: implement more sophisticated data fill methodology such as interpolation
         df['price'] = data['c']
         df['ticker'] = ticker
         df.index.names = ['datetime']
@@ -71,16 +68,16 @@ class DataRetrieverFH(DataRetriever):
 if __name__ == '__main__':
 
     # 1a. test that Data Retriever for Alpha Vantage is working
-    AV_KEY = '00H6LKR56TT7K8VK'
-    AV_URL = 'https://www.alphavantage.co/query?'
-    av = DataRetrieverAV(key=AV_KEY, url=AV_URL, tickers=['IBM', 'AAPL', 'MSFT'], interval='30min')
-    res = av.retrieve()
-    res.to_csv('../data/av_price.csv')
+    # AV_KEY = '00H6LKR56TT7K8VK'
+    # AV_URL = 'https://www.alphavantage.co/query?'
+    # av = DataRetrieverAV(key=AV_KEY, url=AV_URL, interval='30min')
+    # res = av.process_data(av.retrieve('IBM'), 'IBM')
+    # res.to_csv('../data/av_price.csv')
 
     # 1b. test that Finn Hub works, too
-    # FH_TOKEN = 'cnltsshr01qut4m3uve0cnltsshr01qut4m3uveg'
-    # FH_URL = 'https://finnhub.io/api/v1/quote?'
-    # fh = DataRetrieverFH(key=FH_TOKEN, url=FH_URL, tickers=['IBM', 'AAPL', 'MSFT'], interval='30min')
-    # res = fh.retrieve()
-    # res.to_csv('../data/fh_price.csv')
+    FH_TOKEN = 'cnltsshr01qut4m3uve0cnltsshr01qut4m3uveg'
+    FH_URL = 'https://finnhub.io/api/v1/quote?'
+    fh = DataRetrieverFH(key=FH_TOKEN, url=FH_URL, interval='30min')
+    res = fh.process_data(fh.retrieve('IBM'), 'IBM')
+    res.to_csv('../data/fh_price.csv')
 
---
+++ unittest/data_retriever_test.py
@@ -0,0 +1,71 @@
+import unittest
+from src.data_retriever import DataRetrieverAV, DataRetrieverFH
+AV_KEY = 'AC0GWZFNLV7MF4Z0'
+AV_URL = 'https://www.alphavantage.co/query?'
+FH_URL = 'https://finnhub.io/api/v1/quote?'
+FH_TOKEN = 'cnltsshr01qut4m3uve0cnltsshr01qut4m3uveg'
+SAVE_PATH = '../test_output'
+
+
+class DataRetrieverTest(unittest.TestCase):
+    """ DataRetriever Unit Tests"""
+    def setUp(self):
+        self.av = DataRetrieverAV(AV_KEY, AV_URL, '30min')
+        self.fh = DataRetrieverFH(FH_TOKEN, FH_URL, '30min')
+
+    def testAVURL(self):
+        """ Test URL Building """
+        url = self.av.build_url('IBM')
+        self.assertIn(AV_URL, url, 'Base URL Not found in Result')
+        self.assertIn('apikey=' + AV_KEY, url, 'api key Not found in Result')
+        self.assertIn('symbol=IBM', url, 'symbol Not found in Result')
+        self.assertIn('interval=30min', url, 'interval Not found in Result')
+        self.assertIn('outputsize=full', url, 'Base URL Not found in Result')
+        self.assertIn('function=TIME_SERIES_INTRADAY', url, 'api key Not found in Result')
+        self.assertIn('extended_hours=false', url, 'symbol Not found in Result')
+
+    def testFHURL(self):
+        """ Test FH URL Building"""
+        url = self.fh.build_url('IBM')
+        self.assertIn(FH_URL, url, 'Base URL Not found in Result')
+        self.assertIn('token=' + FH_TOKEN, url, 'api key Not found in Result')
+        self.assertIn('symbol=IBM', url, 'symbol Not found in Result')
+        self.assertIn('interval=30min', url, 'interval Not found in Result')
+
+    def testAVData(self):
+        """ Test AV Data Processing """
+        self.av_raw_data = self.av.retrieve('IBM')
+        self.assertIn("Time Series (30min)", self.av_raw_data.keys())
+        self.assertIsNotNone(self.av_raw_data['Time Series (30min)'])
+
+        self.av_data = self.av.process_data(self.av_raw_data, 'IBM')
+        self.assertFalse(self.av_data.empty)
+        self.assertIn('datetime', self.av_data.index.names)
+        self.assertIn('price', self.av_data.columns)
+        self.assertIn('ticker', self.av_data.columns)
+
+    def testFHData(self):
+        """ Test FH Data Processing """
+        self.fh_raw_data = self.fh.retrieve('IBM')
+        self.assertIn("c", self.fh_raw_data.keys())
+        self.assertIsNotNone(self.fh_raw_data['c'])
+        self.assertIsInstance(self.fh_raw_data['c'], float)
+        self.assertIsInstance(self.fh_raw_data['t'], int)
+
+        self.fh_data = self.fh.process_data(self.fh_raw_data, 'IBM')
+        self.assertFalse(self.fh_data.empty)
+        self.assertIn('datetime', self.fh_data.index.names)
+        self.assertIn('price', self.fh_data.columns)
+        self.assertIn('ticker', self.fh_data.columns)
+
+    def tearDown(self):
+        """ Save test data for diagnostics """
+
+
+if __name__ == '__main__':
+    t = DataRetrieverTest()
+    t.testFHURL()
+    t.testAVURL()
+    t.testAVData()
+    t.testFHData()
+    t.tearDown()
